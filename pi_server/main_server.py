a410bf9470fcd4ff083ee09a88856a0ee23cff9a
danhcon123 <gauvatich123@gmail.com>
2024-07-25 15:08:14 +0200

    Add all files from pi_server to pi_server directory

---
+++ pi_server/main_server.py
@@ -0,0 +1,23 @@
+import subprocess
+import time
+
+# Define the full paths to the scripts
+script1 = "/home/admin/Desktop/Metadatas_MQTT_Subscriber.py"
+script2 = "/home/admin/Desktop/Videodatas_UDP.py"
+
+# Start script1 with sudo
+process1 = subprocess.Popen(["sudo", "python3", script1])
+
+# Start script2 without sudo
+process2 = subprocess.Popen(["python3", script2])
+
+# Keep the main script running
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    # Handle cleanup if necessary
+    print("Terminating scripts...")
+    process1.terminate()
+    process2.terminate()
+    print("Scripts terminated")
